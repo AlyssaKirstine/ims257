d700d1949c04a9023460245116b16c39ca0eefda
bkolde <benkolde@Bens-MacBook-Pro-2.local>
2017-12-05 15:10:14 -0500

    updated header

--- helper.js
+++ helper.js
@@ -1,11 +1,8 @@
-import $ from 'jquery'
-import Util from './util'
-
 var cart = 0;
 
     function addToCart(cost) {
         this.cart = this.cart + cost;
-        document.getElementById(myCart).innerHTML = "$" + this.cart;
+        document.getElementById("myCart").innerHTML = "$" + this.cart;
         console.log(this.cart);
     } // end addToCart function
         
