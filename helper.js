c0e1046319c91fa3d37ad139f9f2431d96784efa
Alyssa Melendez <alyssakirstine@gmail.com>
2017-12-05 15:34:47 -0500

    updated headers

--- helper.js
+++ helper.js
@@ -2,6 +2,7 @@ var cart = 0;
 
     function addToCart(cost) {
         this.cart = this.cart + cost;
+        document.getElementById("myCart").innerHTML = "$" + this.cart.toFixed(2);
         document.getElementById("myCart").innerHTML = "$" + this.cart;
         console.log(this.cart);
     } // end addToCart function
